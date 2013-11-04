b6dd8cfe2865a922e8f37693a81f141f1e6288c9
Dave Makena <davemakena@gmail.com>
2013-11-03 20:12:36 -0800

    add test and code

--- test/html_radar_test.rb
+++ test/html_radar_test.rb
@@ -1,6 +1,23 @@
 require 'test_helper'
 
 class HtmlRadarTest < ActiveSupport::TestCase
+
+  test "HtmlRadar refresh url" do
+    memory = "<p><a href='test.html'>Static</a></p><!-- split_tag -->"
+    assert_no_match "Static", HtmlRadar.refresh(memory, "test.html", "a")
+    assert_match "Dynamic", HtmlRadar.refresh(memory, "test.html", "a")
+  end
+
+  test "HtmlRadar show url" do
+    memory = "<p><a href='test.html'>Dynamic</a></p><!-- split_tag --><p><a href='test.html'>Static</a></p>"
+    assert_no_match "Static", HtmlRadar.refresh(memory, "test.html", "a")
+    assert_match "Dynamic", HtmlRadar.refresh(memory, "test.html", "a")
+  end
+
+  test "HtmlRadar.hi" do
+    assert_equal "Hello World!", HtmlRadar.hi
+  end
+
   test "truth" do
     assert_kind_of Module, HtmlRadar
   end
